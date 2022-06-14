034f77ec56ba0e3e7e6be122249dbe59bbb7e3f6
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-14 15:50:15 +0200

    somma tra le variabili

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const numberKm = parseInt(prompt('Quanti kilometri vuoi percorrere?'));
 const userAge = parseInt(prompt('Quanti anni hai?'));
+const pricePerKm = 0.21;
+let ticketPrice = numberKm + userAge + pricePerKm;
 
-// if (userAge < 18)
+console.log(ticketPrice);
