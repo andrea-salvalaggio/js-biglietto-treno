7bc6e59ab932a862c0ca4c806a75685d10039963
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-14 17:55:22 +0200

    sconto processato

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ const userAge = parseInt(prompt('Quanti anni hai?'));
 const pricePerKm = 0.21;
 let ticketPrice = (numberKm * pricePerKm);
 
-// if (userAge < 18) {
-//     ticketPrice = ticketPrice - (ticketPrice - 20 / 100);
+// (Se) Prima condizione: minore di 18 anni
+if (userAge < 18) {
+    ticketPrice = ticketPrice - (ticketPrice * 20 / 100);
+}
 
-// } else (userAge > 65)
-// ticketPrice = ticketPrice - (100 / 40);
+// (Altrimenti se) Seconda condizione: over 65 anni
+else if (userAge > 65) {
+    ticketPrice = ticketPrice - (ticketPrice * 40 / 100);
+}
 
-
-console.log(ticketPrice);
+console.log(ticketPrice.toFixed(2));
