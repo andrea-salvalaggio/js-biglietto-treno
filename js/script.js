8ecaad39f1b7cab182e8cf9f0583eca24ac010c0
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-14 17:33:03 +0200

    costo base

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const numberKm = parseInt(prompt('Quanti kilometri vuoi percorrere?'));
 const userAge = parseInt(prompt('Quanti anni hai?'));
 const pricePerKm = 0.21;
-let ticketPrice = numberKm + userAge + pricePerKm;
+let ticketPrice = (numberKm * pricePerKm);
+
+// if (userAge < 18) {
+//     ticketPrice = ticketPrice - (ticketPrice - 20 / 100);
+
+// } else (userAge > 65)
+// ticketPrice = ticketPrice - (100 / 40);
+
 
 console.log(ticketPrice);
